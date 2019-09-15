ebc7f095bd6824fab759aa06d5ae48ff860123fb
Rumit Man Tandukar <rumit0270@gmail.com>
2019-09-15 18:25:02 +0545

    use the pretrained Resnet50 model to classify the picked image

--- Learn_coreML/MainViewController.swift
+++ Learn_coreML/MainViewController.swift
@@ -37,8 +37,38 @@ class MainViewController: UIViewController, UIImagePickerControllerDelegate, UIN
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
         if let selectedImage = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
             imageView.image = selectedImage
+            
+            guard let ciImage = CIImage(image: selectedImage) else {
+                fatalError("Could not convert the UIImage to CIImage!")
+            }
+            
+            classifyImage(ciImage)
         }
         picker.dismiss(animated: true, completion: nil)
+    }
+    
+    private func classifyImage(_ image: CIImage) {
+        guard let model = try? VNCoreMLModel(for: Resnet50().model) else {
+            fatalError("Error initializing the model!")
+        }
+        
+        // create a vision CoreML request
+        let request = VNCoreMLRequest(model: model) { (request, error) in
+            guard let results = request.results as? [VNClassificationObservation] else {
+                fatalError("Could not get the VNCoreMLRequest")
+            }
+            
+            print(results)
+        }
+        
+        let handler = VNImageRequestHandler(ciImage: image)
+        
+        do {
+            try handler.perform([request])
+        } catch {
+            print(error)
+        }
+        
     }
     
 }
